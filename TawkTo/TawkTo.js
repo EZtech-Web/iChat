3a19f1a36a90e914e7dcb0ef3e9815c2e697b9a9
EZtech Triple-O <104881989+EZtech-Web@users.noreply.github.com>
2024-04-14 12:27:14 +0700

    Update TawkTo.js

--- TawkTo/TawkTo.js
+++ TawkTo/TawkTo.js
@@ -15,7 +15,7 @@
 			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-main.js',
 			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-vendor.js',
 			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-chunk-vendors.js',
-			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-chunk-common.js',
+			'https://ichat.qr-ez.net/TawkTo/TawkToApp.js',
 			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-runtime.js',
 			'https://embed.tawk.to/_s/v4/app/660e2260004/js/twk-app.js'
 		];
